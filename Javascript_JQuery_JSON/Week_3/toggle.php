8ecc8db4dbee85724caae1b80b7a27d6e3e7b731
Subham <subhambh.2000@gmail.com>
2020-08-12 13:28:57 +0530

    jQuery#4

--- Javascript_JQuery_JSON/Week_3/toggle.php
+++ Javascript_JQuery_JSON/Week_3/toggle.php
@@ -1,3 +1,5 @@
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+crossorigin="anonymous"></script>
 <p id="para">Where is the spinner?
 <img id="spinner" src="spinner.gif" height="25"
     style="vertical-align: middle; display: none;">
